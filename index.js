d8d482363d3cd415c91f67e36e1b353bb3f5e384
Kevin Aragon <aragoncodes@gmail.com>
2019-10-30 20:35:13 -0400

    Added scripts for deplyment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ message: 'hi' });
+	res.send({ message: 'Hola Melissa' });
 });
 
 const PORT = process.env.PORT || 5000;
